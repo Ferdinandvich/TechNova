f1a0a89c29d26e95b82bb23132ad71498b590369
Ferdinandvich <ftellmann@mail.com>
2025-01-08 11:55:47 -0300

    Add files via upload

--- techNova/core/urls.py
+++ techNova/core/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
-    path('cursos', views.cursos, name='cursos'),
-    path('servicos', views.servicos, name='servicos'),
-    path('registrar-usuario', views.registrarUsuario, name='registrar-usuario')
+    path('registrar-usuario', views.registrarUsuario, name='registrar-usuario'),
+    path('logout', views.custom_logout, name='deslogar-usuario')
 ]
--- techNova/core/views.py
+++ techNova/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Servico, Curso
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,13 +13,14 @@ def sobre(request):
 def contato(request):
     return render(request, 'contato.html')
 
-def cursos(request):
-    cursos = Curso.objects.all()
-    return render(request, 'cursos.html', {'cursos': cursos})
-
-def servicos(request):
-    servicos = Servico.objects.all()  # Busca todos os registros da tabela Servico
-    return render(request, 'servicos.html', {'servicos': servicos})
-
 def registrarUsuario(request):
+    if request.method == 'POST':
+        nome_usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        user = User.objects.create_user(username=nome_usuario, password=senha)
+        user.save()
     return render(request, 'registrar-usuario.html')
+
+def custom_logout(request):
+    logout(request)
+    return render(request, 'home.html')
